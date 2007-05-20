dab06f436e2d35fb4b0c8ea3147639b4c67bf60d
grahn <grahn>
2007-05-20 20:58:31 +0000

    fix stupidity, and the in-container assertions

--- testicle.h
+++ testicle.h
@@ -12,55 +12,57 @@ namespace testicle {
     void assert_(bool val) {
 	if(val) return;
 	throw AssertionError();
-    };
+    }
 
     template<class M, class N>
     void assert_eq(const M& m, const N& n) {
 	if(m == n) return;
 	throw AssertionError();
-    };
+    }
 
     template<class M, class N>
     void assert_neq(const M& m, const N& n) {
 	if(m != n) return;
 	throw AssertionError();
-    };
+    }
 
     template<class M, class N>
     void assert_lt(const M& m, const N& n) {
 	if(m < n) return;
 	throw AssertionError();
-    };
+    }
 
     template<class M, class N>
     void assert_gt(const M& m, const N& n) {
 	if(m > n) return;
 	throw AssertionError();
-    };
-
-    template<class T, class C>
-    void assert_in(const C& container, const T& t) {
-	if(container.find(t) != container.end()) return;
-	throw AssertionError();
-    };
+    }
 
     template<class T, class Iter>
     void assert_in(Iter first, Iter last, const T& t) {
 	if(std::find(first, last, t) != last) return;
 	throw AssertionError();
-    };
+    }
 
     template<class T, class C>
-    void assert_not_in(const C& container, const T& t) {
-	if(container.find(t) == container.end()) return;
-	throw AssertionError();
-    };
+    void assert_in(const C& container, const T& t) {
+	assert_in(container.begin(),
+		  container.end(),
+		  t);
+    }
 
     template<class T, class Iter>
     void assert_not_in(Iter first, Iter last, const T& t) {
 	if(std::find(first, last, t) == last) return;
 	throw AssertionError();
-    };
+    }
+
+    template<class T, class C>
+    void assert_not_in(const C& container, const T& t) {
+	assert_not_in(container.begin(),
+		      container.end(),
+		      t);
+    }
 }
 
 #endif
