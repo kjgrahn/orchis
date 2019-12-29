dccc4f4c54cbdd9576e04a3b70992a21ddfb5451
Jörgen Grahn <grahn+src@snipabacken.se>
2019-12-30 00:18:53 +0100

    class Function, class Tests

--- tests.cc
+++ tests.cc
@@ -9,6 +9,52 @@
 #include "split.h"
 
 #include <iostream>
+#include <algorithm>
+
+namespace {
+
+    bool starts_with(const std::string& haystack,
+		     const std::string& needle)
+    {
+	auto i = std::search(begin(haystack), end(haystack),
+			     begin(needle), end(needle));
+	return i==begin(haystack);
+    }
+}
+
+Function::Function(const std::string& s)
+    : kind(0)
+{
+    auto lpar = std::find(begin(s), end(s), '(');
+    ns_f = {begin(s), lpar};
+    const std::string arg {lpar, end(s)};
+    ns = split("::", ns_f);
+    if (ns.empty()) return;
+    fun = ns.back();
+    ns.pop_back();
+
+    if (arg=="") {
+	if (starts_with(fun, "test_")) kind = 'c';
+    }
+    else if (arg=="()") {
+	if (starts_with(fun, "test_")) kind = 'O';
+    }
+    else if (arg=="(orchis::TC)") {
+	kind = 'o';
+    }
+}
+
+bool Function::is_test() const
+{
+    switch (kind) {
+    case 'o':
+    case 'O':
+    case 'c':
+	return true;
+    default:
+	return false;
+    }
+}
 
 namespace {
 
--- tests.h
+++ tests.h
@@ -11,16 +11,36 @@
 #include <string>
 
 /**
+ * A T symbol as seen by ld(1).
+ */
+class Function {
+public:
+    explicit Function(const std::string& s);
+    bool is_test() const;
+
+    std::string ns_f;
+    std::vector<std::string> ns;
+    std::string fun;
+    char kind;
+};
+
+/**
  * Tests, as found as symbols in a set of object files or archives.
  *
- * test_foo T                 C test
- * foo::bar(orchis::TC) T     normal C++ test
- * foo::test_bar() T          old-style C++ test
+ * test_foo               C test
+ * foo::bar(orchis::TC)   normal C++ test
+ * foo::test_bar()        old-style C++ test
  */
 class Tests {
 public:
     Tests(std::ostream& err, const std::vector<std::string>& objects);
     bool bad = false;
+
+    std::vector<Function>::const_iterator begin() const { return tt.begin(); }
+    std::vector<Function>::const_iterator end() const { return tt.end(); }
+
+private:
+    std::vector<Function> tt;
 };
 
 void generate(std::ostream& os,
